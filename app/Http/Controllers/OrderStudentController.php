d30e298b09c8e6ceefd35739270ecf63de331531
Vishanuel <vishanuel@gmail.com>
2020-06-16 00:17:56 +1200

    bug fixes

--- app/Http/Controllers/OrderStudentController.php
+++ app/Http/Controllers/OrderStudentController.php
@@ -214,7 +214,7 @@ class OrderStudentController extends Controller
 		
 		DB::table('cos_order')
 			->insert([
-				['Student_ID' => $student_id->Student_ID, 'Cos_Meal_Date_Time' => $request->input('meal_date') , 'Cos_Order_Date_Time' => date("Y-m-d"), 'Cos_Order_Meal_Status' => 'orderingg', 'Cos_Order_Cost' => $total_cost, 'Cos_Order_Payment_Method' => 'NN'],
+				['Student_ID' => $student_id->Student_ID, 'Cos_Meal_Date_Time' => $request->input('meal_date') , 'Cos_Order_Date_Time' => date("Y-m-d"), 'Cos_Order_Meal_Status' => 'orderingg', 'Cos_Order_Cost' => $total_cost, 'Cos_Order_Payment_Method' => 'NN', 'Menu_ID' => $menuid],
 		]);	
 	
 		
@@ -1744,19 +1744,17 @@ class OrderStudentController extends Controller
     public function edit($orderid)
     {
 		
-		$menuid=DB::table('ordered_food_item')	
-		->join('menu_food','menu_food.Menu_Food_Item_ID','=','ordered_food_item.Menu_Food_Item_ID')
-		->join('menu','menu_food.Menu_ID','=','menu.Menu_ID')
-		->where('Cos_Order_Num','=',$orderid)
-		->first();
-		
-		$menuid=$menuid->Menu_ID;
-
-		
 		$student_id = DB::table('student')
 		->select('Student_ID')
 		->where('User_ID','=',Auth::user()->id)
 		->first();
+		
+		$menuid=DB::table('cos_order')	
+		->where('Student_ID','=',$student_id->Student_ID)
+        ->where('Cos_Order_Num', '=', $orderid)
+		->first();
+		
+		$menuid=$menuid->Menu_ID;
 		
 		$cos_order=DB::table('cos_order')
 		->where('Student_ID','=',$student_id->Student_ID)

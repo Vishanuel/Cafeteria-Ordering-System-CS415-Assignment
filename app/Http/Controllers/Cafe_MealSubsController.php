d9b9a65d378d07ff8a92102a48b4d8d36be1556f
John Sade <61434827+Sade10k@users.noreply.github.com>
2020-06-17 10:52:06 +1200

    Added controller files for the new blades

--- app/Http/Controllers/Cafe_MealSubsController.php
+++ app/Http/Controllers/Cafe_MealSubsController.php
@@ -100,15 +100,17 @@ class Cafe_MealSubsController extends Controller
 		
 		
 		
-		$SD_Instruction_ID = DB::table('subscription_delivery_instruction')->where('MealSubs_ID','=',$id)->first();
+		$SD_Instruction_ID = DB::table('patron_subscription_delivery_instruction')
+		->where('MealSubs_ID','=',$id)
+		->first();
 		
 
 		
 
 		DB::table('meal_subscription')->where('MealSubs_ID','=',$id)->update(['Meal_Status'=>'Pending Delivery']);
-		DB::statement("ALTER TABLE `subscription_delivery_request` AUTO_INCREMENT = 1;");
-		DB::table('subscription_delivery_request')->insert(['Subscription_Delivery_ID' => $SD_Instruction_ID->Subscription_Delivery_ID]);
-		return redirect('/cafeteria')->with('success', 'Delivery request sent');
+		DB::statement("ALTER TABLE `patron_subscription_delivery_request` AUTO_INCREMENT = 1;");
+		DB::table('patron_subscription_delivery_request')->insert(['Patron_Subscription_Delivery_Instruction_ID'=>$SD_Instruction_ID->Patron_Subscription_Delivery_Instruction_ID]);
+		return redirect('/cafe_subs')->with('success', 'Delivery request sent');
 	}
 	
 	public function deliv_request($id)
@@ -128,7 +130,119 @@ class Cafe_MealSubsController extends Controller
 		
 		DB::table('cos_order')->where('Cos_Order_Num','=',$id)->update(['Cos_Order_Meal_Status'=>'Pending Delivery']);
 		DB::table('delivery_request')->insert(['D_Instruction_ID' => $D_Instruction_ID->D_Instruction_ID, 'Meal_Deliverer_ID'=>$meal_deliverer_id->Meal_Deliverer_ID]);
-		return redirect('/cafeteria')->with('success', 'Delivery request sent');
+		return redirect('/cafe_subs')->with('success', 'Delivery request sent');
+	}
+
+	public function process_payment($id){
+        try{
+			
+			$meal_sub=DB::table('meal_subscription')
+			//->where('Employee_ID','=',$employee_id->Employee_ID)
+			->where('MealSubs_ID','=',$id)
+			->first();
+
+			$employee_id=DB::table('meal_subscription')
+				//->where('Employee_ID','=',$employee_id->Employee_ID)
+				->select('Employee_ID')
+				->where('MealSubs_ID','=',$id)
+				->first();
+
+		
+			if($meal_sub->Meal_Subscription_Payment_Method == "payroll"){
+				$pay = DB::table('payroll')
+				->select('Salary')
+				->where('Employee_ID','=',$meal_sub->Employee_ID)
+				->first();
+				
+				$salary = $pay->Salary;
+			
+				$meal_sub=DB::table('meal_subscription')
+				->where('Employee_ID','=',$employee_id->Employee_ID)
+				//->where('Cos_Order_Meal_Status','=','Editing')
+				->first();
+					
+				
+				if(!empty($meal_sub) && $meal_sub->Meal_Subscription_Payment_Method == "payroll"){
+					
+					$salary = $salary + $meal_sub->Total_Price;
+				}
+				
+				$meal_sub=DB::table('meal_subscription')
+				->where('Employee_ID','=',$employee_id->Employee_ID)
+				->where('MealSubs_ID','=',$id)
+				->first();
+				
+				$salary = $salary - $meal_sub->Total_Price;
+					
+				DB::table('payroll')->where('Employee_ID', '=',$employee_id->Employee_ID)->update(['Salary' => $salary]);
+			
+					
+			}
+			if($meal_sub->Meal_Subscription_Payment_Method == "card"){
+
+			    $employee_id=DB::table('meal_subscription')
+				//->where('Employee_ID','=',$employee_id->Employee_ID)s				
+				->where('MealSubs_ID','=',$id)
+				->select('Employee_ID')
+				->first();
+
+				//$employee_id=DB::table('meal_subscription')
+				//->where('Employee_ID','=',$employee_id->Employee_ID)
+				//->select('Employee_ID')
+				//->where('MealSubs_ID','=',$id)
+				//->first();
+
+				$card = DB::table("card_payment")
+				->select('Card_Number')
+				->where('Employee_ID','=',$employee_id->Employee_ID)
+				->first();
+				
+				$card_balance = DB::table('card_bank')
+				->select('Card_Balance')
+				->where('Card_Number','=',$card->Card_Number)
+				->first();
+				
+				$card_bal=$card_balance->Card_Balance;
+				
+				$meal_sub=DB::table('meal_subscription')
+				->where('Employee_ID','=',$employee_id->Employee_ID)
+				//->where('Cos_Order_Meal_Status','=','Editing')
+				->first();
+				
+				if(!empty($meal_sub) && $meal_sub->Meal_Subscription_Payment_Method == "card"){
+					$card_bal = $card_bal + $meal_sub->Total_Price;
+				}
+				
+				$meal_sub=DB::table('meal_subscription')
+				->where('Employee_ID','=',$employee_id->Employee_ID)
+				->where('MealSubs_ID','=',$id)
+				->first();
+				
+				$card_bal = $card_bal - $meal_sub->Total_Price;
+				
+				$error = "Insufficient funds.Cannot Process Payment.";
+
+				if(($card_bal - $meal_sub->Total_Price) <= 0){
+					
+					return view('cafe_subs')->with('error', $error);
+				}
+				else{
+					DB::table('card_bank')->where('Card_Number', '=',$card->Card_Number)->update(['Card_Balance' => $card_bal]);
+					
+				}
+			}
+			DB::table('meal_subscription')->where('MealSubs_ID','=',$id)->update(['Paid'=> 1]);
+			return redirect('cafe_subs')->with('success','Payment Processed!');
+			
+		}
+		catch(exception $e){
+			return redirect('cafe_subs')->with('error','Unable to Process Payment\n'.$e);
+		}
+		
+		//DB::statement("UPDATE 'meal_subscription' SET 'Paid' = 1 WHERE 'MealSubs_ID' = ?,[$id];");
+
+
+	//return redirect('/cafeteria')->with('success', 'Payment Processed!');
 	}
 }
 
